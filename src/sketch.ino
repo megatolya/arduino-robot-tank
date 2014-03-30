d8c47c0a6ce1c0630803962f0244d2ea7cb96ef5
Anatoliy Ostrovskiy <forgetthisbox@gmail.com>
2014-03-30 22:06:22 +0400

    cc

--- src/sketch.ino
+++ src/sketch.ino
@@ -16,25 +16,23 @@ void loop()
     //stop="s"=115
     //right="r"=114
     //left="l"=108
+
     int serialVal = Serial.read();
     if (serialVal != prevSerialVal) {
         prevSerialVal = serialVal;
         if (serialVal == 114) {
             servoVal = 1;
-            change = true;
         }
 
         if (serialVal == 108) {
-            change = true;
             servoVal = 179;
         }
 
         if (serialVal == 115) {
-            change = true;
             servoVal = 90;
         }
 
-        //  servoVal = map(xVal, 0, 1023, 0, 180);
+        // servoVal = map(xVal, 0, 1023, 0, 180);
 
         servo.write(servoVal);
     }
